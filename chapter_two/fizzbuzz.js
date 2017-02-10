a736c62e9d6daebc8644edb8a3dc458385eca1e3
SARA SIMON <smbsimon@gmail.com>
2017-02-10 15:55:03 -0500

    add fizzbuzz solution

---
+++ chapter_two/fizzbuzz.js
@@ -0,0 +1,11 @@
+// solution one
+for (var n = 1; n <= 100; n++) {
+  if (n % 3 == 0 && n % 5 == 0)
+    console.log("FizzBuzz")
+  else if (n % 3 == 0)
+    console.log("Fizz")
+  else if (n % 5 == 0)
+    console.log("Buzz")
+  else
+    console.log(n)
+}
